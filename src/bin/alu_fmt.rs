d86cad37551c0db5151acf7cf5011f3fec59b9ce
Alu <davidalujones@gmail.com>
2026-07-26 21:05:23 -0700

    chore(bootstrap): Execute final purge of Rust scaffolding

--- src/bin/alu_fmt.rs
+++
@@ -1,7 +0,0 @@
-// ST-05: Official Tooling (alu fmt)
-
-fn main() {
-    println!("ALU Auto-Formatter");
-    println!("Standardizing spaces, brackets, and indentation for all .alu files...");
-    // Parses the AST and re-emits it according to standard style guide
-}
